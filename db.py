e08d222bb5790841f05465973561cc9e4e12136f
Kevin Vongmany <kevin.vongmany@gmail.com>
2025-04-25 12:41:17 +1000

    Updated db handlers for UserDB class to fetch user ids from usernames

--- db.py
+++ db.py
@@ -1,7 +1,13 @@
 import json
 import os
-from config import USERS_DB, BRICK_DB, DICE_DB
+from config import USERS_DB, BRICK_DB, DICE_DB, CLIENT_ID, CLIENT_SECRET
 from datetime import datetime
+from twitchapi import TwitchAPI
+
+import logging
+
+# Set up logging
+logger = logging.getLogger(__name__)
 
 class JSONDatabase:
     """
@@ -60,6 +66,8 @@ class UserDatabase(JSONDatabase):
 
     def __init__(self):
         super().__init__(USERS_DB, self.DEFAULT_DATA)
+        self.twitch_api = TwitchAPI(CLIENT_ID, CLIENT_SECRET)
+
 
     def update_user_data(self, user_id, payload):
         for user in self.data["users"]:
@@ -102,7 +110,13 @@ class UserDatabase(JSONDatabase):
         for user in self.data["users"]:
             if user["name"] == username:
                 return user["id"]
-        return None
+        try:
+            user_data = self.twitch_api.make_request("users", params={"login": username})
+            user_id = user_data["data"][0]["id"]
+        except (KeyError, IndexError):
+            logger.error(f"Failed to get user ID for {username}: {user_data}")
+            user_id = None
+        return user_id
     
     def grant_permamod(self, user_id) -> None:
         self.update_user_data(user_id, {"mod": True, "persistent_mod": True})
